6110df679211ee9044a2c6296e6f25bc7a5ce0be
KMartin27 <58310987+MARTIN535718@users.noreply.github.com>
2023-11-04 16:37:46 +0100

    Add files via upload

---
+++ Naptar/java.js
@@ -0,0 +1,6 @@
+function refreshTime() {
+    var datetime = new Date().toLocaleTimeString();
+    console.log(datetime); // it will represent date in the console of developers tool
+    document.getElementById("time").textContent = datetime; // represent on html page
+}
+    setInterval(refreshTime, 1000);
